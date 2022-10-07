56633954d85cc98318c9f0bb59cca53a2d1a8a5d
Diego Galiotti <diegaliotti@gmail.com>
2022-10-07 19:21:41 -0300

    se agrega el promedio

--- mayor-menor/mayor-menor/Form1.cs
+++ mayor-menor/mayor-menor/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace mayor_menor
 {
     public partial class Form1 : Form
@@ -5,6 +7,7 @@ namespace mayor_menor
         public Form1()
         {
             InitializeComponent();
+            
         }
         //cargar numeros+
         private void mayorMenor()
@@ -21,8 +24,7 @@ namespace mayor_menor
             lista[0] = numero1;
             lista[1] = numero2;
             lista[2] = numero3;
-            double a = lista[0];
-
+            
             double max = lista[0];
             double min = lista[0];
 
@@ -32,18 +34,43 @@ namespace mayor_menor
                     max = lista[i];
                 else if (lista[i] < min)
                     min = lista[i];
-
             }
             MessageBox.Show("El numero más chico que subiste es: " + min + " y el más alto es: " + max );
-
-
-
         }
 
+        private void promedio()
+        {
+            string t1 = textBox1.Text;
+            string t2 = textBox2.Text;
+            string t3 = textBox3.Text;
+
+            double numero1 = Double.Parse(t1);
+            double numero2 = Double.Parse(t2);
+            double numero3 = Double.Parse(t3);
+
+            double[] lista = new double[3];
+            lista[0] = numero1;
+            lista[1] = numero2;
+            lista[2] = numero3;
+
+            double prom = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                //MessageBox.Show("el valores promedio es " + prom);
+                prom = (prom + lista[i]);
+                //MessageBox.Show("el valor que esta poninedo es es " + lista[i]);
+                
+            }
+
+            double promedio = prom / 3;
+            MessageBox.Show("el promedio de tus numeros es: " + promedio);
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
             mayorMenor();
+            promedio();
         }
 
         private void label1_Click(object sender, EventArgs e)
