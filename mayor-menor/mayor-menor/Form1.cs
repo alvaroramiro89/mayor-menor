2534adbaafc57cf9313404a36ed1337046e850b2
DiegoGaliotti <diegaliotti@gmail.com>
2022-10-09 20:12:12 -0300

    Se crea función parImpar

--- mayor-menor/mayor-menor/Form1.cs
+++ mayor-menor/mayor-menor/Form1.cs
@@ -69,8 +69,34 @@ namespace mayor_menor
             MessageBox.Show("el promedio de tus numeros es: " + promedio);
         }
 
+        private void parImpar()
+        {
+            string t1 = textBox1.Text;
+            string t2 = textBox2.Text;
+            string t3 = textBox3.Text;
+
+            double numero1 = Double.Parse(t1);
+            double numero2 = Double.Parse(t2);
+            double numero3 = Double.Parse(t3);
+
+            double[] lista = new double[3];
+            lista[0] = numero1;
+            lista[1] = numero2;
+            lista[2] = numero3;
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                double resto = lista[i] % 2;
+                if (resto == 0)
+                    MessageBox.Show("Su número es par");
+                else
+                    MessageBox.Show("su numero es impar");
+
+            };
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            parImpar();
             mayorMenor();
             promedio();
         }
